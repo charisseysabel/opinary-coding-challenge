da320763c0e86908348c276c63a20e3710640a3c
Charisse De Torres <charisse.ysabel930@gmail.com>
2022-03-03 18:16:05 +0100

    Move config directory to root

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import './styles.css';
 import { IConfig } from './types';
 
 function app(): void {
-  const config: IConfig = require(`./configs/${__CONFIG}`);
+  const config: IConfig = require(`../configs/${__CONFIG}`);
 
   if (!config) {
     throw new Error(`Config not found.`);
